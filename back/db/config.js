caa9ad6a5b48b30e978baa72804487b87982c929
unknown <camilo.228@hotmail.com>
2021-10-23 17:37:39 -0500

    Agregando NodeJs

---
+++ back/db/config.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');//se importa el archivo de mongosse
+const dbConnection = async () => { //Creamos una función para almacenar la conexión
+    try {
+        await mongoose.connect(process.env.BD_CONNECTION);
+        console.log("Estoy conectado");
+    } catch (error) {
+        console.log("Error");
+    }
+    
+    
+
+}
+
+module.exports =  dbConnection;
